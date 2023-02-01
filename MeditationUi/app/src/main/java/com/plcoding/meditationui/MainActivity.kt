3c99ba70a1322446b6ee8c3de0e2d665d707bfb4
ElavarasanRobosoft <elavarasan.t@robosoftin.com>
2023-02-01 11:48:54 +0530

    ui

---
+++ MeditationUi/app/src/main/java/com/plcoding/meditationui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.plcoding.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import com.plcoding.meditationui.ui.HomeScreen
+import com.plcoding.meditationui.ui.theme.MeditationUIYouTubeTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
